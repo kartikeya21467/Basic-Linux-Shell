ca6f04832f8de860a876d7bb9e39f9c77ab5e9ee
kartikeya21467 <108337746+kartikeya21467@users.noreply.github.com>
2022-12-24 15:33:10 +0530

    Update mkdir.c

--- mkdir.c
+++ mkdir.c
@@ -7,26 +7,6 @@
 #include<dirent.h>
 
 #define TOK_DELIM " /\t\r\n\a"
-// char **dir_names(char * nm){
-//     int count=0;
-//     for(int i=0;i<strlen(nm);i++){
-//         if(nm[i]=='/'){
-//             count++;
-//         }
-//     }
-//     count++;
-//     char **tokens = malloc(count * sizeof(char *));
-//     int pos=0;
-//     char *token = strtok(nm,TOK_DELIM);
-//     while(token!=NULL){
-//         printf("%s\n",token);
-//         tokens[pos++]=token;
-//         token=strtok(NULL,TOK_DELIM);
-//     }
-//     for(int i=0;i<pos;i++){
-//         printf("%s  ",tokens[i]);
-//     }
-// }
 
 int main(int argc, char **args){
     if(!strcmp(args[1],".")){
@@ -46,15 +26,11 @@ int main(int argc, char **args){
     }
     else if(!strcmp(args[1],"-p")){
         int count=0;
-        // for(int i=0;i<strlen(nm);i++){
-        //     if(!strcmp(nm[i],"/")){
-        //         count++;
-        //     }
-        // }
+        
         count++;
-        // printf("%s\n","Hello");
+        
         char **tokens = malloc(100 * sizeof(char *));
-        // printf("%s\n","Hello");
+        
         int pos=0;
         const char s[2]=" ";
         char *token = strtok(args[2],TOK_DELIM);
@@ -64,9 +40,7 @@ int main(int argc, char **args){
             tokens[pos++]=token;
             token=strtok(NULL,TOK_DELIM);
         }
-        // for(int i=0;i<pos;i++){
-        //     printf("%s  ",tokens[i]);
-        // }
+        
         for(int i=0;i<pos;i++){
             int ch = chdir(tokens[i]);
             if(ch!=0){
